726858825e2631dfd89155d6007009b9de6528af
Adam0019 <aniruddhamallick2021@gmail.com>
2025-04-18 20:22:13 +0530

    db update

--- Database/ams.sql
+++ Database/ams.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3308
--- Generation Time: Apr 09, 2025 at 07:04 PM
+-- Generation Time: Apr 18, 2025 at 04:51 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
